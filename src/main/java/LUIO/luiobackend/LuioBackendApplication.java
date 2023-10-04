c59b23f88c89f0ae95f8292c31ee6de281abc484
mistarson <yeowuli2@naver.com>
2023-10-04 16:59:48 +0900

    Initial commit

---
+++ src/main/java/LUIO/luiobackend/LuioBackendApplication.java
@@ -0,0 +1,13 @@
+package LUIO.luiobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/LUIO/luiobackend/LuioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package LUIO.luiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
